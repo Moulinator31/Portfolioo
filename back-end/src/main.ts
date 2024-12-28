9fa4516b2d02f6ac5c7b345417fb955773abbe9b
Elsa Tanguy <elsa.tang28@gmail.com>
2024-12-28 18:40:08 +0100

    modifications for docker

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,28 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+const port = process.env.PORT || 8000
+console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('api/v1');  // Préfixe global pour toutes les routes
-
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://portfolioo-22.vercel.app'], // Ajoutez vos URLs autorisées
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Méthodes HTTP autorisées
-    allowedHeaders: 'Content-Type, Accept', // En-têtes autorisés
-    credentials: true, // Permet les cookies et les sessions
+    origin: ['http://localhost:3000','https://portfolioo-22.vercel.app/'],  // Frontend URL
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
   });
-
-  const port = process.env.PORT || 8000;
-  console.log(`Server running on http://localhost:${port}`);
-  await app.listen(port);
+  await app.listen(port)
 }
 
 bootstrap();
-
 
 
 
