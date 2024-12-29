acb2bc7d4b9b67eeb62ec5e97e3167a4c4d2c99a
Elsa Tanguy <elsa.tang28@gmail.com>
2024-12-29 13:07:52 +0100

    test

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');  // Préfixe global pour toutes les routes
   app.enableCors({
-    origin: ['https://portfolioo-22.vercel.app','http://localhost:3000',],  // Frontend URL
+    origin: ['https://elsa-11yn6cb4e-moulinator31s-projects.vercel.app','http://localhost:3000','https://protective-barbee-elsa-tanguy-projects-1a18647e.koyeb.app/api/v1','protective-barbee-elsa-tanguy-projects-1a18647e.koyeb.app/'],  // Frontend URL
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
