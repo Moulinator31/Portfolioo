f0fc7dfd71b7d3754c0ff9777c79aa9299583573
Elsa Tanguy <elsa.tang28@gmail.com>
2025-01-10 12:35:17 +0100

    modifications pour les dossiers .env local et production back-end

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,21 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const port = process.env.PORT || 8000
-console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
+import * as dotenv from 'dotenv';
+
+// Charger les variables d'environnement basées sur l'environnement
+const ENV = process.env.NODE_ENV || 'local'; // Par défaut, "local"
+dotenv.config({ path: ENV === 'production' ? '.env' : '.env.local' });
+
+const port = process.env.PORT || 8000;
+
+console.log(`Launching NestJS app on port ${port}, ENV: ${ENV}, URL: http://0.0.0.0:${port}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');  // Préfixe global pour toutes les routes
   app.enableCors({
-    origin: ['https://portfolioo-22.vercel.app','http://localhost:3000','http://localhost:3001','https://protective-barbee-elsa-tanguy-projects-1a18647e.koyeb.app','https://elsatanguy.com/'],  // Frontend URL
+    origin: process.env.FRONTEND_URL.split(','),  // Autoriser plusieurs URL
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
   });
-  await app.listen(port)
+
+  await app.listen(port);
 }
 
 bootstrap();
+
 
 
 
